9ca06ffd34dd7883ac963838a0630335f48fb72c
BaptouK <baptiste.dupuis2406@gmail.com>
2025-04-01 14:09:03 +0200

    Fixed urls

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://baptouk.github.io/docusaurus/",
+  url: "https://baptouk.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docusaurus",
+  baseUrl: "/docusaurus/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
